dd4264b0ae5182af56186eea539fc645276c1a19
Gregory Dutton <greg.dutton@gmail.com>
2023-05-24 17:11:09 -0700

    lab in progress V2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let addToy = false;
+let likeButtons;
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -12,4 +13,66 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  loadToys();
 });
+
+document.addEventListener('click', event => {
+  let likeButton = event.target;
+  if (likeButton.className === 'like-btn') {
+    let numLikes = likeButton.parentNode.querySelector('p').innerText;
+    numLikes = numLikes.substr(0, numLikes.indexOf(' '));
+    updateLikes(numLikes, likeButton.id);
+  }
+});
+
+function loadToys() {
+  fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    data.forEach(element => {
+      let toy = document.createElement('div');
+      toy.className = 'card';
+      toy.id = element.name;
+      const toyHeader = document.createElement('h2');
+      toyHeader.innerHTML = element.name;
+      const toyImg = document.createElement('img');
+      toyImg.src = element.image;
+      toyImg.className = 'toy-avatar'
+      const toyLikes = document.createElement('p');
+      toyLikes.innerHTML = `${element.likes} Likes`;
+      const toyButton = document.createElement('button');
+      toyButton.className = 'like-btn';
+      toyButton.id = element.id;
+      toyButton.innerText = 'Like ❤️';
+      document.getElementById('toy-collection').appendChild(toy);
+      toy.appendChild(toyHeader);
+      toy.appendChild(toyImg);
+      toy.appendChild(toyLikes);
+      toy.appendChild(toyButton);
+    });
+    likeButtons = document.querySelectorAll('.like-btn');
+  });
+  }
+
+  function updateLikes(numLikes, btnID) {
+  // console.log(`http://localhost:3000/toys/${btnID}`);
+  let newLikes = numLikes + 1;
+  const field = "likes";
+  const obj = {};
+  obj.likes = newLikes;
+  console.log(obj);
+  fetch(`http://localhost:3000/toys/${btnID}`, {
+  method: 'PATCH',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({obj})
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // document.getElementById(NEED THE ID)
+  });
+  }
